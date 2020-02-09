2e803e56d55d046a4f006cb46e241df2c8951f27
Michael Grant <aubie87@gmail.com>
2020-02-09 17:57:35 -0600

    Add project files.

---
+++ AdWorksCore3.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdWorksCore3.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
